392d7717b7e451ecbc5bd5dde1da6e0d7fd2aa77
Ragne <me@lain.im>
2018-12-28 01:41:55 +0300

    Blocking server (bad sleepy thing). Mio should be better. Also, bad thread pool(disabled by default)

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,43 @@
 use std::io;
 use std::io::prelude::*;
-use std::net::{Shutdown, TcpListener, TcpStream};
+use std::net::{Shutdown, TcpStream};
+use std::sync::atomic::{AtomicBool, Ordering};
+use std::sync::Arc;
+use std::thread;
+use std::time::Duration;
+mod server_blocking;
+extern crate ctrlc;
+
+#[macro_use]
+extern crate log;
+extern crate chrono;
+extern crate fern;
 
 mod threadpool;
 extern crate dockworker;
 
-use dockworker::{Docker, container::ContainerFilters, ContainerListOptions};
+use dockworker::{container::ContainerFilters, Docker};
 
-fn handle_client(mut stream: TcpStream) {
+pub fn setup_logger() -> Result<(), fern::InitError> {
+    fern::Dispatch::new()
+        .format(|out, message, record| {
+            out.finish(format_args!(
+                "{}[{}][{}] {}",
+                chrono::Local::now().format("[%Y-%m-%d %H:%M:%S]"),
+                record.target(),
+                record.level(),
+                message
+            ))
+        })
+        .level(log::LevelFilter::Warn)
+        .level_for("docker_check", log::LevelFilter::Trace)
+        .chain(std::io::stdout())
+        .chain(fern::log_file("output.log")?)
+        .apply()?;
+    Ok(())
+}
+
+fn handle_client(mut stream: TcpStream, finished: &Arc<AtomicBool>) {
     // ...
     let mut should_stop = false;
     let response = format!(
@@ -16,26 +46,30 @@ fn handle_client(mut stream: TcpStream) {
          Content-Length: 0{ending}{ending}",
         ending = "\r\n"
     );
+    trace!("in handle_client!");
     loop {
         let mut buf = vec![0; 512];
         match stream.read(&mut buf) {
             Ok(read_bytes) => {
-                if read_bytes == 0 {
+                if read_bytes == 0 || read_bytes < buf.len() {
                     should_stop = true
                 }
-                println!("read {} bytes!", read_bytes);
-                println!(
+                trace!("read {} bytes!", read_bytes);
+                trace!(
                     "trying to decode: {:?}\n",
                     String::from_utf8_lossy(&buf[0..read_bytes])
                 );
+                if String::from_utf8_lossy(&buf[0..read_bytes]).contains("stop") {
+                    finished.store(true, Ordering::SeqCst);
+                }
                 match stream.write(response.as_bytes()) {
-                    Ok(bytes_written) => println!("wrote {} bytes!", bytes_written),
+                    Ok(bytes_written) => debug!("wrote {} bytes!", bytes_written),
                     Err(ref e) => match e.kind() {
                         std::io::ErrorKind::UnexpectedEof | std::io::ErrorKind::BrokenPipe => {
                             should_stop = true
                         }
                         x => {
-                            println!("stream.read::Error: {:?}", x);
+                            warn!("stream.read::Error: {:?}", x);
                             should_stop = true
                         }
                     },
@@ -47,54 +81,51 @@ fn handle_client(mut stream: TcpStream) {
                     should_stop = true
                 }
                 x => {
-                    println!("stream.read::Error: {:?}", x);
+                    warn!("stream.read::Error: {:?}", x);
                     should_stop = true
                 }
             },
         };
 
-        println!("there!!!");
         if should_stop {
-            println!("EOF reached!");
+            debug!("EOF reached or stop was signalled explicitly!");
             stream.shutdown(Shutdown::Both).unwrap_or(());
             break;
         }
     }
 }
 
-use std::thread;
-use std::time::Duration;
-
-fn listen() -> io::Result<()> {
-    let listener = TcpListener::bind("127.0.0.1:8000")?;
-    let pool = threadpool::ThreadPool::new(5);
-    // accept connections and process them serially
-    thread::spawn(move || {
-        for stream in listener.incoming() {
-            pool.execute(|| {
-                handle_client(stream.unwrap());
-            });
-        }
-    });
+fn listen(finished: Arc<AtomicBool>) -> io::Result<()> {
     let docker = Docker::connect_with_http("http://localhost:2376").unwrap();
-    
 
-    let filter = ContainerFilters::new();
-    let containers = docker.list_containers(None, None, None, filter).unwrap();
-
-    loop {
-            containers.iter().for_each(|c| {
-                let res = docker.container_info(&c).unwrap();
-                    println!("{:?}", res.State.Health.Status);
-            });
-        thread::sleep(Duration::from_secs(10));
-
-
+    while !finished.load(Ordering::Relaxed) {
+        let filter = ContainerFilters::new();
+        let containers = docker.list_containers(None, None, None, filter).unwrap();
+        containers.iter().for_each(|c| {
+            let res = docker.container_info(&c).unwrap();
+            debug!("{:?}: {:?}", res.Name, res.State.Health.Status);
+        });
+        thread::sleep(Duration::from_secs(2));
     }
     Ok(())
 }
 
 fn main() {
-    println!("Hello, world!");
-    listen().unwrap();
+    setup_logger().unwrap();
+
+    let mut s = server_blocking::Server::new("127.0.0.1:8000", 0);
+    let f = s.get_finished();
+    let f2 = s.get_finished();
+    ctrlc::set_handler(move || {
+        f2.store(true, Ordering::SeqCst);
+    })
+    .expect("Error setting Ctrl-C handler");
+    thread::spawn(move || {
+        s.serve(handle_client);
+    });
+
+    listen(f.clone()).unwrap();
+    info!("Done");
+    info!("Stopping~!");
+    f.store(true, Ordering::SeqCst);
 }
